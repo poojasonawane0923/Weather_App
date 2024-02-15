e7db84f97a5997a73763da6e9cb3933028bfabaa
poojasonawane0923 <POOJAVSONAWANE09@GMAIL.COM>
2024-02-15 14:27:58 +0530

    updated

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 async function fetchWeatherData(cityName)
 {
-	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=d42d6485b91d83ba7031f5b5d5dad931`
+	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d42d6485b91d83ba7031f5b5d5dad931`
 const response= await fetch(url)
 const data= await response.json();
-console.log(data)
+let city = document.getElementById("city");
+city.innerHTML = data.name;
+let windSpeed = document.getElementById("wind_speed");
+windSpeed.innerHTML = data.wind.speed + "km/hr";
+let humidity=document.getElementById("humidity")
+humidity.innerHTML =data.main.humidity +" g/kg";
+let temp=document.getElementById("temp");
+temp.innerHTML= Math.round((data.main.temp) -273.15)+"°C";
+console.log(data);
 }
-// demo()
-function fetchcity(){
-	let cityName=document.getElementById("city_name");
+
+function fetchCity() 
+{
+	let cityName = document.getElementById("cityName");
 	if(cityName.value===""){
-		alert("enter a city name");
+		alert("Enter a city name");
 	}
 	else{
 		fetchWeatherData(cityName.value);
